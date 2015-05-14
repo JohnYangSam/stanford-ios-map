35320eca515c709c0ce3f0c2ceab569afff44647
John YS <johnys@stanford.edu>
2015-05-14 12:32:53 -0700

    Initial Commit

---
+++ StanfordMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StanfordMap
+//
+//  Created by John YS on 5/14/15.
+//  Copyright (c) 2015 Silicon Valley Insight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
