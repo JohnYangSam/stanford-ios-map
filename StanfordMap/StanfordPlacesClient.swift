24d976cda9607bbdd854bb27f0d4c3f1c045e9d1
John YS <johnys@stanford.edu>
2015-05-21 21:35:46 -0700

    commit partway work on grabbing images

--- StanfordMap/StanfordPlacesClient.swift
+++ StanfordMap/StanfordPlacesClient.swift
@@ -12,28 +12,49 @@ import CheatyXML
 
 class StanfordPlacesClient: NSObject {
     
-    let baseString = "http://campus-map.stanford.edu/bldg_xml.cfm"
+    // Base string for searching buildings
+    let baseSearchString = "http://campus-map.stanford.edu/bldg_xml.cfm"
+  
+    let searchManager:AFHTTPRequestOperationManager
+    let searchRequestSerializer:AFHTTPRequestSerializer
+    let searchResponseSerializer:AFHTTPResponseSerializer
+    
+    // Base string for searching specific building information
+    let baseReportString = "http://campus-map.stanford.edu/report-cm.cfm"
+    
+    let reportManager:AFHTTPRequestOperationManager
+    let reportRequestSerializer:AFHTTPRequestSerializer
+    let reportResponseSerializer:AFHTTPResponseSerializer
     
     let userAgentString = "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
    
-    let manager:AFHTTPRequestOperationManager
-    let requestSerializer:AFHTTPRequestSerializer
-    let responseSerializer:AFHTTPResponseSerializer
-    
     override init() {
         
-        requestSerializer = AFHTTPRequestSerializer()
-        requestSerializer.setValue("User-Agent", forHTTPHeaderField: userAgentString)
+        // Search
+        searchRequestSerializer = AFHTTPRequestSerializer()
+        searchRequestSerializer.setValue("User-Agent", forHTTPHeaderField: userAgentString)
         
-        responseSerializer = AFHTTPResponseSerializer()
-        responseSerializer.acceptableContentTypes = Set(["text/xml"])
+        searchResponseSerializer = AFHTTPResponseSerializer()
+        searchResponseSerializer.acceptableContentTypes = Set(["text/xml"])
         
-        var url:NSURL = NSURL(string: baseString)!
-        manager = AFHTTPRequestOperationManager(baseURL: url)
+        var searchURL:NSURL = NSURL(string: baseSearchString)!
+        searchManager = AFHTTPRequestOperationManager(baseURL: searchURL)
         
-        manager.requestSerializer = requestSerializer
-        manager.responseSerializer = responseSerializer
+        searchManager.requestSerializer = searchRequestSerializer
+        searchManager.responseSerializer = searchResponseSerializer
         
+        // Reports
+        reportRequestSerializer = AFHTTPRequestSerializer()
+        reportRequestSerializer.setValue("User-Agent", forHTTPHeaderField: userAgentString)
+        
+        reportResponseSerializer = AFHTTPResponseSerializer()
+        reportResponseSerializer.acceptableContentTypes = Set(["text/html"])
+        
+        var reportURL:NSURL = NSURL(string: baseReportString)!
+        reportManager = AFHTTPRequestOperationManager(baseURL: reportURL)
+        
+        reportManager.requestSerializer = reportRequestSerializer
+        reportManager.responseSerializer = reportResponseSerializer
         
         super.init()
     }
@@ -44,7 +65,7 @@ class StanfordPlacesClient: NSObject {
         var params = NSMutableDictionary()
         params.setValue(searchTerm, forKey: "srch")
         
-        manager.GET("",
+        searchManager.GET("",
             parameters: params,
             success:{(operation: AFHTTPRequestOperation!, responseObject: AnyObject!)in
                 
@@ -78,6 +99,30 @@ class StanfordPlacesClient: NSObject {
                 completion(buildings: nil, error: error)
         })
         
+    }
+    
+    func getBuildingReportWithCompletion(buildingId: String, completion: (imageString:String?, error:NSError) -> Void) {
+        var params = NSMutableDictionary()
+        params.setValue(buildingId, forKey: "id")
+        
+        // Call the Stanford Campus Map API
+        reportManager.GET("",
+            parameters: params,
+            success:{(operation: AFHTTPRequestOperation!, responseObject: AnyObject!)in
+                
+                var data: NSData? = responseObject as? NSData
+                if (data != nil) {
+                    // NEED TO PARSE HTML FOR THE PICTURE HERE
+                }
+            },
+            
+            failure:{(operation: AFHTTPRequestOperation!, error: NSError!)in
+                
+                println("Error: "+error.localizedDescription)
+                
+                completion(imageString: nil, error: error)
+        })
+ 
     }
     
     // This makes recursive calls to the API until we get all results, then it will deduplicate them
